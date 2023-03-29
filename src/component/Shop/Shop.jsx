9c9162fec487138a6ec5d70f7b70e852ee6fd0ca
MHSourov621 <mhsourov621@gmail.com>
2023-03-29 15:49:56 +0600

    set cart state from localstorage

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -14,9 +15,26 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        let saveCart = [];
+
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+
+                saveCart.push(addedProduct);
+            }
+        }
+        setCart(saveCart);
+    }, [products]);
+
     const handleAdToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
     return (
         <div>
